f63d459be949be7b74efb431737013c8ac3d699a
Jonmorais <jonathan_ms15@hotmail.com>
2021-10-18 12:40:45 -0300

    Crud Class

---
+++ CRUD/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO sakila.staff 
+    (first_name, last_name, address_id, email, store_id, active, username, password)
+VALUES 
+    ('Jonathan', 'Morais', 5, 'jonathan@hotmail.com', 2 , 1, 'Jonathan', 'jonathan' );
+
+
+INSERT INTO `sakila`.`staff`
+    (first_name, last_name, address_id, email, store_id, active, username, password)
+VALUES
+    ('Geralt', 'of Rivia', 2, 'tossacoin@gmail.com', 1, 1, 'geralt', 'theWhiteWolf');
+
+INSERT INTO `sakila`.`staff`
+    (first_name, last_name, address_id, email, store_id, active, username, password)
+VALUES
+    ('Dovahkiin', 'of Skyrim', 6, 'dragonborn@gmail.com', 1, 1, 'dovahkiin', 'theDragonBorn'),
+    ('Gabriel', 'Belmont', 7, 'monsterhunter@gmail.com', 2, 1, 'gabriel', 'theDracula');
+
