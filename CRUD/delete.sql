c50c0a82af23f1414b0d3ac2c1343268c7d8da8b
Jonmorais <jonathan_ms15@hotmail.com>
2021-10-18 15:18:26 -0300

    Exercices about how to Insert, Update and Delete data on tables.

---
+++ CRUD/delete.sql
@@ -0,0 +1,6 @@
+-- DELETE FROM banco_de_dados.tabela
+-- WHERE coluna = 'valor';
+-- O WHERE é opcional. Porém, sem ele, todas as linhas da tabela seriam excluídas.
+
+DELETE FROM sakila.film_text
+WHERE title = 'ACADEMY DINOSAUR';
